09eed385325cb131798746b663494f9c692e43d0
muhzii <airomyst517@gmail.com>
2019-12-26 21:02:44 +0200

    Implement the path planning strategy

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,11 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
+
+#define LEFT_LANE 0
+#define CENTER_LANE 1
+#define RIGHT_LANE 2
 
 // for convenience
 using nlohmann::json;
@@ -50,8 +55,13 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+  int car_lane = 1;
+  double ref_vel = 0.0;
+
+  const double MAX_SPEED = 49.5;
+
+  h.onMessage([&ref_vel, &car_lane, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
+               &map_waypoints_dx,&map_waypoints_dy, &MAX_SPEED]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -88,16 +98,180 @@ int main() {
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
 
-          json msgJson;
+          int previous_path_size = previous_path_x.size();
+
+          // For more correct calculation of the next path we want
+          // to base those calculation of the car's positioning
+          // on the last point of our previous path
+          // (in case we didn't already consume it).
+          if (previous_path_size > 0) {
+            car_s = end_path_s;
+          }
+
+          // Check other cars through the sensor fusion data
+          // to determine if there's a car nearby.
+          bool car_ahead = false;
+          bool car_left = false;
+          bool car_right = false;
+          for (int i = 0; i < sensor_fusion.size(); i++) {
+            float d = sensor_fusion[i][6];
+            int other_car_lane = -1;
+
+            // Determine car lane
+            if (d > 0 && d < 4) {
+              other_car_lane = LEFT_LANE;
+            } else if (d > 4 && d < 8) {
+              other_car_lane = CENTER_LANE;
+            } else if (d > 8 && d < 12) {
+              other_car_lane = RIGHT_LANE;
+            }
+            if (other_car_lane < 0) {
+              continue;
+            }
+
+            // Determine car speed from vx, vy
+            double vx = sensor_fusion[i][3];
+            double vy = sensor_fusion[i][4];
+            double other_car_speed = sqrt(vx*vx + vy*vy);
+            double other_car_s = sensor_fusion[i][5];
+
+            // Estimate the car's s position after consuming the previous
+            // path to match with our own car.
+            other_car_s += ((double)previous_path_size * 0.02 * other_car_speed);
+
+            // We check if there are any nearby cars within our path's distance
+            if (other_car_lane == car_lane) {
+              car_ahead |= other_car_s > car_s && other_car_s - car_s < 35;
+            } else if (other_car_lane - car_lane == -1) {
+              car_left |= car_s - 35 < other_car_s && car_s + 35 > other_car_s;
+            } else if (other_car_lane - car_lane == 1) {
+              car_right |= car_s - 35 < other_car_s && car_s + 35 > other_car_s;
+            }
+          }
+
+          const double MAX_ACC_ENC = .224;
+
+          // Determine if we should increase or decrease the speed and whether
+          // or not we should switch lanes.
+          double car_acceleration = 0;
+          if (car_ahead) {
+            // Change lane to get over the car ahead of us
+            if (!car_left && car_lane != LEFT_LANE) {
+              car_lane--;
+            } else if (!car_right && car_lane != RIGHT_LANE){
+              car_lane++;
+            } else {
+              car_acceleration -= MAX_ACC_ENC;
+            }
+          } else {
+            if (car_lane != 1) {
+              if ((car_lane == LEFT_LANE && !car_right) || (car_lane == RIGHT_LANE && !car_left))
+                car_lane = 1;
+            }
+
+            if (ref_vel < MAX_SPEED) {
+              car_acceleration += MAX_ACC_ENC;
+            }
+          }
+
+          // We want to create a spline to model the trajectory of the next path
+          // using a couple of few last points in the previous path and few
+          // future points based on the coordinates and directionality of our
+          // car to be able to get values of interploated points.
+          vector<double> spline_x_vals;
+          vector<double> spline_y_vals;
+
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+
+          if (previous_path_size < 2) {
+              double previous_car_x = ref_x - cos(car_yaw);
+              double previous_car_y = ref_y - sin(car_yaw);
+
+              spline_x_vals.push_back(previous_car_x);
+              spline_y_vals.push_back(previous_car_y);
+
+              spline_x_vals.push_back(ref_x);
+              spline_y_vals.push_back(ref_y);
+          } else {
+              ref_x = previous_path_x[previous_path_size-1];
+              ref_y = previous_path_y[previous_path_size-1];
+
+              double previous_ref_x = previous_path_x[previous_path_size-2];
+              double previous_ref_y = previous_path_y[previous_path_size-2];
+              ref_yaw = atan2(ref_y - previous_ref_y, ref_x - previous_ref_x);
+
+              spline_x_vals.push_back(previous_ref_x);
+              spline_x_vals.push_back(ref_x);
+
+              spline_y_vals.push_back(previous_ref_y);
+              spline_y_vals.push_back(ref_y);
+          }
+
+          // Using 3 more future points for the spline calculation at a far distance.
+          double car_next_d = 2 + 4 * car_lane;
+          vector<double> fp0 = getXY(car_s + 30, car_next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> fp1 = getXY(car_s + 60, car_next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> fp2 = getXY(car_s + 90, car_next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          spline_x_vals.push_back(fp0[0]);
+          spline_y_vals.push_back(fp0[1]);
+
+          spline_x_vals.push_back(fp1[0]);
+          spline_y_vals.push_back(fp1[1]);
+
+          spline_x_vals.push_back(fp2[0]);
+          spline_y_vals.push_back(fp2[1]);
+
+          // Adjusting the coordinates used to create the spline to the
+          // coordinates and yaw of the car after consuming the previous path.
+          for (int i = 0; i < spline_x_vals.size(); i++) {
+            double shift_x = spline_x_vals[i] - ref_x;
+            double shift_y = spline_y_vals[i] - ref_y;
+
+            spline_x_vals[i] = shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw);
+            spline_y_vals[i] = shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
+          }
+
+          // Create the spline.
+          tk::spline spline;
+          spline.set_points(spline_x_vals, spline_y_vals);
 
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
-          /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
+          // Append points from the previous path in the next path for continuity.
+          for (int i = 0; i < previous_path_size; i++) {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
 
+          // Calculate distance y position on 30m ahead on x.
+          double target_x = 30.0;
+          double target_y = spline(target_x);
+          double target_dist = sqrt(target_x*target_x + target_y*target_y);
+
+          double curr_x_disp = 0;
+          for (int i = 1; i < 50 - previous_path_size; i++) {
+            ref_vel += car_acceleration;
+            if (ref_vel > MAX_SPEED) {
+              ref_vel = MAX_SPEED;
+            } else if (ref_vel < MAX_ACC_ENC) {
+              ref_vel = MAX_ACC_ENC;
+            }
+
+            double N = target_dist / (0.02 * ref_vel / 2.24);
+            double x_point = curr_x_disp + target_x/N;
+            double y_point = spline(x_point);
+
+            curr_x_disp = x_point;
+
+            next_x_vals.push_back(x_point * cos(ref_yaw) - y_point * sin(ref_yaw) + ref_x);
+            next_y_vals.push_back(x_point * sin(ref_yaw) + y_point * cos(ref_yaw) + ref_y);
+          }
+
+          json msgJson;
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
